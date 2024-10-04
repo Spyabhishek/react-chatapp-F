cfe5775432278d7890695a8a40d754ef68205057
Abhishek Kumar <kabhishek2023@gmail.com>
2024-10-04 21:52:13 +0530

    Intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-const App = () => {
-  return (
-    <div className=''>App</div>
-  )
-}
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+
+const App = () => (
+  <div className='container'>
+    <List />
+    <Chat />
+    <Detail />
+  </div>
+)
 
 export default App
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,76 @@
+import "./detail.css"
+
+const Detail = () => {
+    return (
+        <div className="detail">
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Jane Doe</h2>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img
+                                    src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    alt=""
+                                />
+                                <span>Photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img
+                                    src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    alt=""
+                                />
+                                <span>Photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img
+                                    src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                    alt=""
+                                />
+                                <span>Photo_2024_2.png</span>
+                            </div>
+                            <img src="./download.png" alt="" className="icon" />
+                        </div>
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+                <button className="logout">Logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Detail
